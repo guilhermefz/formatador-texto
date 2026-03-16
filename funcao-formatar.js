8bcc2e3ef6e9c124b76a110589771372250394dc
GUILHERME FIUZA NOLASCO DE CAMPOS <guilherme_f_campos@estudante.sesisenai.org.br>
2026-03-16 20:07:02 -0300

    feat: método de formatação que deixa a primeira letra de cada palavra maiuscúla

--- funcao-formatar.js
+++ funcao-formatar.js
@@ -1,6 +1,25 @@
+var txt = "análise e desenvolvimento de sistemas";
+
 function formatarTexto(texto) {
-  // TODO: implementar
-  return texto;
+  var t = texto;
+  var r = "";
+  var palavras = t.split(" ");
+  
+  for (var i = 0; i <= palavras.length; i++) {
+    var p = palavras[i];
+    
+    if (p != undefined) {
+      var pri = p[0].toUpperCase();
+      var res = pri + p.substring(1, p.length);
+      r = r + res + " ";
+    }
+  }
+
+  r = r.trim();
+  
+  console.log("formatou");
+  
+  return r;
 }
 
 module.exports = formatarTexto;
